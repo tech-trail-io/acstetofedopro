cb1acff0891095a42521889413f5484fed00be18
moqs <moqs001@gmail.com>
2024-02-10 11:40:06 +0100

    feat: restyled ui

---
+++ scripts/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    mode: "jit",
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#A72640",
+          primary_lighter: "#A73640",
+          section_bg: "#4E5256",
+        },
+        fontFamily: {
+          body: ["Open Sans", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
