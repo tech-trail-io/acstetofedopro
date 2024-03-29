ff647dd8d10a9f1d4c79d21fc710e492585e27e6
Gabor Pichner <pgabor@cae.com>
2024-03-29 10:16:18 +0100

    feat(react): init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#A72640',
+      primary_lighter: '#A73640',
+      section_bg: '#4E5256',
+    },
+    fontFamily: {
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@headlessui/tailwindcss')],
+};
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const webpack = require('webpack');
+
+const paths = [
+  {
+    path: '/',
+    lastmod: new Date().toISOString().split('T').shift(),
+  },
+];
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+/** @type {import('webpack').Configuration}*/
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: 'dist',
+    port: 8000,
+    allowedHosts: 'all',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      publicPath: './',
+      minify: isDev
+        ? false
+        : {
+            collapseWhitespace: true,
+            keepClosingSlash: true,
+            removeComments: true,
+            removeRedundantAttributes: false,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true,
+          },
+    }),
+    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/robots.txt', to: './' },
+        { from: './src/assets', to: './assets/' },
+      ],
+    }),
+    new SitemapPlugin({ base: 'https://acstetofedo.hu/', paths }),
+    new CssMinimizerPlugin(),
+    new webpack.ProgressPlugin(),
+  ],
+};
